e769e9b0919318fe363a7189e66d77f010b49943
PaiSillanpaa <sillanpaapai@gmail.com>
2025-03-27 14:32:52 +0200

    tehtävä 3.3 tehty

--- index.js
+++ index.js
@@ -37,6 +37,17 @@ app.get("/", (request, response) => {
   `);
 });
 
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
